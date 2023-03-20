8dae640ad140baed74d5030eb2ac5013482dcd9a
Lam Huynh <thanhlam0290@gmail.com>
2023-03-21 01:07:12 +0700

    update home

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
@@ -6,11 +7,21 @@ import { AuthService } from '@auth0/auth0-angular';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
   isLoggedIn: boolean = false;
-  constructor(private auth: AuthService) {
+  constructor(private auth: AuthService, private router: Router) {
+    
+  }
+  ngOnInit(): void {
     this.auth.user$.subscribe((user) => {
       this.isLoggedIn = user !== null;
+      this.redirect();
     });
+    this.redirect();
+  }
+  redirect(){
+    if(this.isLoggedIn){
+      this.router.navigate(['feed']);
+    }
   }
 }
